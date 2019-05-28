0f69445cdfab2a978d0cd1df4136c84d81f68748
Ricardo Lence <lencericardo@gmail.com>
2019-05-28 13:19:19 +0200

    estructura del chat html

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ app.set('port', process.env.PORT || 3000);
 //static files
 app.use(express.static(path.join(__dirname, 'public') ));
 
-
 // start the server
 const server = app.listen(app.get('port'), ()=>{
        console.log('server on port', app.get('port'));
 });
 
+
 //stting socketIo
-const io = SocketIO(server);
+const io = SocketIO(server);//pasando el server a socket ya que necesita de un servidor levantado para funcionar
 
 //websockets
-io.on('connection', ()=> {
-       console.log('new connection')
+io.on('connection', (socket)=> {
+       console.log('new connection', socket.id)//socket id te da un id diferente por cada conexion
 });
 
 
