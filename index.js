f3719c1db74b89a7b9ef03d0bb7191e8153901c1
Ricardo Lence <lencericardo@gmail.com>
2019-05-28 13:05:08 +0200

    aplicate websocket

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const SocketIO = require('socket.io');
 
 //settings
-
 app.set('port', process.env.PORT || 3000);
+
+//static files
+app.use(express.static(path.join(__dirname, 'public') ));
 
 
 // start the server
-app.listen(app.get('port'), ()=>{
+const server = app.listen(app.get('port'), ()=>{
        console.log('server on port', app.get('port'));
 });
+
+//stting socketIo
+const io = SocketIO(server);
+
+//websockets
+io.on('connection', ()=> {
+       console.log('new connection')
+});
+
+
